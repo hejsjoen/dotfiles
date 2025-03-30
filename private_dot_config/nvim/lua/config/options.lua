e1cb71ef133efb1591fac8c32b8672fb51f8bf60
hejsjoen <132153083+hejsjoen@users.noreply.github.com>
2025-03-30 23:09:48 +0200

    Add dotnet nvim

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,123 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+    local blink_ok, blink_cmp = pcall(require, "blink.cmp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_ok and cmp_nvim_lsp.default_capabilities() or blink_ok and blink_cmp.get_lsp_capabilities() or {}
+    )
+    require("mason").setup({
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:crashdummyy/mason-registry",
+      },
+    })
+    require("mason-lspconfig").setup_handlers({
+      function(server)
+        local config = vim.tbl_deep_extend("error", {
+          capabilities = capabilities,
+        }, require("config.lspconfig.settings")[server] or {})
+
+        -- Something weird with rust-analyzer and nvim-cmp capabilites
+        -- Makes the completion experience awful
+        if server == "rust_analyzer" and cmp_ok then
+          config.capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), {
+            resolveSupport = {
+              properties = {
+                "additionalTextEdits",
+                "textEdits",
+                "tooltip",
+                "location",
+                "command",
+              },
+            },
+          })
+        end
+
+        require("lspconfig")[server].setup(config)
+      end,
+    })
+  end,
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          { path = "${3rd}/busted/library" },
+          { path = "${3rd}/luassert/library" },
+          { path = "snacks.nvim", words = { "Snacks" } },
+          { path = "nvim-test" },
+        },
+      },
+    },
+    { "b0o/schemastore.nvim" },
+    {
+      "williamboman/mason.nvim",
+      config = true,
+      cmd = "Mason",
+      dependencies = { "roslyn.nvim" },
+      registries = { "github:Crashdummyy/mason-registry" },
+    },
+    { "williamboman/mason-lspconfig.nvim", config = true, cmd = { "LspInstall", "LspUninstall" } },
+    { "seblyng/nvim-lsp-extras", opts = { global = { border = CUSTOM_BORDER } }, dev = true },
+    { "onsails/lspkind.nvim" },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    keys = { "<leader>d<leader>", "<leader>db" },
+    dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      ---@diagnostic disable-next-line: missing-fields
+      dapui.setup({
+        mappings = {
+          edit = "i",
+          remove = "dd",
+        },
+      })
+
+      local function keymap(mode, lhs, rhs, opts)
+        opts.desc = string.format("Dap: %s", opts.desc)
+        vim.keymap.set(mode, lhs, function()
+          rhs()
+          vim.fn["repeat#set"](vim.keycode(lhs))
+        end, opts)
+      end
+
+      vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Error", linehl = "", numhl = "" })
+      vim.fn.sign_define("DapStopped", { text = "→", texthl = "Error", linehl = "DiffAdd", numhl = "" })
+
+      keymap("n", "<leader>db", dap.toggle_breakpoint, { desc = "Add breakpoint" })
+      keymap("n", "<leader>d<leader>", dap.continue, { desc = "Continue debugging" })
+      keymap("n", "<leader>dl", dap.step_into, { desc = "Step into" })
+      keymap("n", "<leader>dj", dap.step_over, { desc = "Step over" })
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      -- Per language config
+      require("config.dap.cs").setup()
+      require("config.dap.rust").setup()
+    end,
+  },
+}
--- private_dot_config/nvim/lua/plugins/dotnet.lua
+++ private_dot_config/nvim/lua/plugins/dotnet.lua
@@ -1,28 +1,13 @@
 return {
-    "seblyng/roslyn.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        filewatching = "off",
-        broad_search = true,
-        ignore_target = function(sln)
-            return string.match(sln, "SmartDok.sln") ~= nil
-        end,
-    },
-    dev = true,
-    init = function()
-        vim.keymap.set("n", "<leader>ds", function()
-            if not vim.g.roslyn_nvim_selected_solution then
-                return vim.notify("No solution file found")
-            end
-
-            local projects = require("roslyn.sln.api").projects(vim.g.roslyn_nvim_selected_solution)
-            local files = vim.iter(projects)
-                :map(function(it)
-                    return vim.fs.dirname(it)
-                end)
-                :totable()
-
-            Snacks.picker.files({ dirs = files })
-        end)
+  "seblyng/roslyn.nvim",
+  ft = "cs",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {},
+  {
+    "GustavEikaas/easy-dotnet.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("easy-dotnet").setup()
     end,
+  },
 }
